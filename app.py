d904d486b8131f8dcff708f5e82dfe5cf126aa99
Raahim Fareed <raahimfareed@proton.me>
2024-05-22 21:07:15 +0500

    init

---
+++ app.py
@@ -0,0 +1,36 @@
+import sys
+import os
+import marko
+from flask import Flask, render_template
+
+args = sys.argv
+
+if len(args) < 2:
+    print("Please provide a file name to parse")
+    exit(1);
+
+file_name = args[1]
+
+if not os.path.isfile(file_name) or not file_name.lower().endswith('.md'):
+    print("Please provide a valid file to parse")
+    exit(1)
+
+try:
+    with open(file_name, 'r', encoding='utf-8') as file:
+        contents = file.read()
+except Exception as e:
+    print(f"An error occurred while reading the file: {e}")
+    exit(1)
+
+
+print(marko.convert(contents))
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
---
+++ sample-markdown/hello.md
@@ -0,0 +1,9 @@
+# Hello, World!
+
+This is a sample markdown file to test the parsing
+
+*This should be italic*
+
+**This should be bold**
+
+***And this should be italic and bold***
